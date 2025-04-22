9312e5f52b8dfc7e284c34d03aed9f24b1ea0d48
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-04-22 13:31:21 -0300

    feat: add info.js

---
+++ data/info.js
@@ -0,0 +1,17 @@
+export const memes = [
+    {
+        id: 1,
+        title: "Club 57",
+        image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.wikipedia.org%2Fwiki%2FClub_57&psig=AOvVaw0jltScvORo2QRP89a8IklA&ust=1745425257301000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCOj_xKCG7IwDFQAAAAAdAAAAABAW",
+    },
+    {
+        id: 2,
+        title: "Club 57 - Temp 1",
+        image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcoverykids.fandom.com%2Fpt-br%2Fwiki%2FClub_57&psig=AOvVaw0jltScvORo2QRP89a8IklA&ust=1745425257301000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCOj_xKCG7IwDFQAAAAAdAAAAABAE",
+    },
+    {
+        id: 3,
+        title: "Club 57 - Temp 2",
+        image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Ffebreteen.com.br%2F2a-temporada-club-57%2F&psig=AOvVaw0jltScvORo2QRP89a8IklA&ust=1745425257301000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCOj_xKCG7IwDFQAAAAAdAAAAABAQ",
+    }
+]
